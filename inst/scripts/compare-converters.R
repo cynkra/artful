57f22af72f0d71d8d75f73e7cd7dc43c234c82cd
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-05-19 12:38:46 +0200

    Compare converter options

---
+++ inst/scripts/compare-converters.R
@@ -0,0 +1,38 @@
+# ------------------------------------------------------------------------------
+# Aim: compare solutions to convert RTF tables to data frames
+# ------------------------------------------------------------------------------
+
+# Simple RTF table -------------------------------------------------------------
+library(gt)
+library(unrtf)
+pkgload::load_all()
+
+file_rtf <- tempfile(fileext = ".rtf")
+
+gt(head(penguins)) |>
+  tab_header(md("**Title**")) |>
+  tab_footnote(md("_footnote 1_<br>footnote 2")) |>
+  gtsave(file_rtf)
+
+file_artful <- tempfile(fileext = ".html")
+file_unrtf <- tempfile(fileext = ".html")
+file_knitr <- knitr::pandoc(file_rtf, "html")
+
+html_artful <- rtf_to_html(file_rtf)
+html_unrtf <- unrtf::unrtf(file_rtf, "html")
+html_knitr <- readLines(file_knitr)
+
+writeLines(html_artful, file_artful)
+writeLines(html_unrtf, file_unrtf)
+
+waldo::compare(html_artful, html_unrtf)
+waldo::compare(html_artful, html_knitr)
+waldo::compare(html_unrtf, html_knitr)
+
+browseURL(file_artful)
+browseURL(file_unrtf)
+browseURL(file_knitr)
+
+# Summary
+# - Return html tables of differing levels of complexity
+# -
