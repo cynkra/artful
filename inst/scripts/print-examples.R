fa765e1b63e9a38c61e55a919d28c7ea440d16e2
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-05-22 11:18:32 +0200

    Quickly view all examples to generate heuristics

---
+++ inst/scripts/print-examples.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+pkgload::load_all()
+
+load_and_view <- function(file) {
+  rtf_file <- system.file("extdata", file, package = "artful")
+
+  html <- rtf_to_html(rtf_file)
+
+  html |>
+    minimal_html() |>
+    html_element("table") |>
+    html_table() |>
+    View()
+}
+
+walk(
+  paste0("bms-", 1:5, ".rtf"),
+  load_and_view
+)
