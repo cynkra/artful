5f5e0fef1eb39a8e8bc1efd68ebcdad6acf8af2f
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-06-03 11:56:00 +0200

    Update process

--- inst/scripts/example-process.R
+++ inst/scripts/example-process.R
@@ -1,8 +1,26 @@
-library(rvest)
 pkgload::load_all()
 
-rtf_file <- system.file("extdata", "bms-1.rtf", package = "artful")
+# Double indentation
+bms_1 <- system.file("extdata", "bms-1.rtf", package = "artful") |>
+  rtf_to_html() |>
+  html_to_dataframe() |>
+  strip_pagination() |>
+  separate_indentation() |>
+  # custom code
+  mutate(
+    variable = stringr::str_to_upper(paste(variable_label1, variable_label2))
+  ) |>
+  mutate(variable = stringr::str_remove(variable, "FROM ")) |>
+  mutate(variable = stringr::str_replace_all(variable, " ", "_")) |>
+  relocate(variable, .before = "variable_level")
 
-html <- rtf_to_html(rtf_file)
+print(bms_1)
 
-df <- html_to_dataframe(html)
+# Single indentation
+bms_3 <- system.file("extdata", "bms-3.rtf", package = "artful") |>
+  rtf_to_html() |>
+  html_to_dataframe() |>
+  strip_pagination() |>
+  separate_indentation()
+
+print(bms_3)
