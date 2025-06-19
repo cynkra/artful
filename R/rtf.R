8143f36439130e5de75147bafae9a3aae8873f18
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-06-19 16:55:33 +0200

    Fix indentation: - rename function - operate on string rather than file - replace spaces with the same number of non breaking spaces

--- R/rtf.R
+++ R/rtf.R
@@ -1,20 +1,19 @@
-#' Fix Indentation in RTF Files
+#' Fix Indentation issues in RTF encoded strings
 #'
-#' Processes an RTF (Rich Text Format) file to fix indentation issues by
-#' replacing RTF-specific indentation codes and spaces with HTML non-breaking
+#' Processes an RTF (Rich Text Format) encoded string to fix indentation issues
+#' by replacing RTF-specific indentation codes and spaces with HTML non-breaking
 #' space entities.
 #'
-#' @param file A character string specifying the path to the RTF file to be
-#'   processed. The file should be readable and contain valid RTF content.
+#' @param string A character vector specifying the RTF encodings.
 #'
 #' @return A character string containing the processed RTF content with
 #'   indentation codes replaced by HTML non-breaking space entities (`&nbsp;`).
-#'   The original file is not modified; only the processed string is returned.
 #'
 #' @details
 #' The function performs the following transformations:
 #' \itemize{
-#'   \item Replaces spaces immediately after opening braces before `\cell}` with `&nbsp;`
+#'   \item Replaces spaces immediately after opening braces before `\cell}` with
+#'     the same number of `&nbsp;` calls.
 #'   \item Converts `\li192` codes to two non-breaking spaces
 #'   \item Converts `\li384` codes to four non-breaking spaces
 #'   \item Converts `\li576` codes to six non-breaking spaces
@@ -25,13 +24,20 @@
 #'
 #' @examples
 #' \dontrun{
-#' fix_indentation("path/to/your/file.rtf")
+#' rtf_indentation("{  two spaces inside the cell\\cell}")
+#' rtf_indentation("\\li192{two spaces before the cell\\cell}")
+#' rtf_indentation("\\li576{ Seven spaces in total\\cell}")
 #' }
 #'
 #' @export
-fix_indentation <- function(file) {
-  rtf_string <- readr::read_file(file)
-  rtf_string |>
+rtf_indentation <- function(string) {
+  string |>
+    stringr::str_replace_all("(\\{)( +)(.*?\\\\cell\\})", function(match) {
+      groups <- stringr::str_match(match, "(\\{)( +)(.*?\\\\cell\\})")
+      space_count <- nchar(groups[, 3])
+      nbsp_replacement <- paste(rep("&nbsp;", space_count), collapse = "")
+      paste0(groups[, 2], nbsp_replacement, groups[, 4])
+    }) |>
     stringr::str_replace_all("(\\{)( +)(.*?\\\\cell\\})", "\\1&nbsp;\\3") |>
     stringr::str_replace_all("\\\\li192", "&nbsp;&nbsp;") |>
     stringr::str_replace_all("\\\\li384", "&nbsp;&nbsp;&nbsp;&nbsp;") |>
