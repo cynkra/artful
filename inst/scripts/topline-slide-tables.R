c4cdb5485c310d4047b25e49d88bb390cccc638c
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-06-24 13:08:12 +0200

    Save first pass at indentation parser

--- inst/scripts/topline-slide-tables.R
+++ inst/scripts/topline-slide-tables.R
@@ -4,14 +4,23 @@ pkgload::load_all()
 # ---- Slide 7 -----------------------------------------------------------------
 
 # ---- rt-dm-demo.rtf ----
+temp_rtf <- tempfile(fileext = ".rtf")
+system.file("extdata", "rt-dm-demo.rtf", package = "artful") |>
+  read_file() |>
+  rtf_indentation() |>
+  write_file(temp_rtf)
 
-# Indentation levels are lost when calling pandoc for both RTF's on slide 7.
-# This is because the input tables use a mixture of single spaces, double spaces
-# and \li calls to signify indentation in the tables. A solution to replace
-# these spaces with HTML non-breaking spaces has been implemented in R/rtf.R.
-# This solution now needs testing on these files. Once it is confirmed it works,
-# a new algroithm to assign indentation levels (and subsequently create new
-# columns) needs implementing. This new strategy should be more robust.
+rtf_to_html(temp_rtf) |>
+  html_to_dataframe() |>
+  strip_pagination() |>
+  strip_indentation() # New WIP fun
+
+
+separate_indentation()
+pivot_group() |>
+  View()
+
+tibble(nbsp_count = c(0, 4, 4, 4, 0, 2, 2, 2, 4, 4, 0, 6, 0))
 
 # ---- rt-dm-basedz.rtf ----
 
@@ -52,7 +61,26 @@ system.file("extdata", "rt-dm-basedz.rtf", package = "artful") |>
 # 2. Check that rtf_indentation() correctly detects indentation for the tables
 #    listed above (render HTML), and tweak function as necessary. Note: the
 #    RTF files will first need to be read in and converted to a string before
-#    being passed to rtf_indentation(). Use readr::read_file() here.
+#    being passed to rtf_indentation(). Use readr::read_file() here. Code:
+#
+# file_1 <- system.file("extdata", "rt-dm-demo.rtf", package = "artful")
+# file_2 <- system.file("extdata", "rt-dm-basedz.rtf", package = "artful")
+
+# temp_rtf <- tempfile(fileext = ".rtf")
+# temp_html <- tempfile(fileext = ".html")
+
+# read_file(file_2) |>
+#   rtf_indentation() |>
+#   write_lines(temp_rtf)
+
+# rtf_to_html(temp_rtf) |>
+#   write_lines(temp_html)
+
+# browseURL(temp_html)
+#
+#    Confirmed that this works BUT indentation is not consistent. It jumps from
+#    two spaces, to eight. We need some clever parsing algorithm in 3. ✅
+#
 # 3. Fix indentation issue and update parsing algorithm to determine levels
 # 4. Fix offset column issue by manipulating raw RTF file
 # 5. Write stats parsers that will extract stats from the above two examples
@@ -65,3 +93,59 @@ system.file("extdata", "rt-dm-basedz.rtf", package = "artful") |>
 # rt-ef-acr20.rtf
 # rt-ef-aacr50.rtf
 # rt-ef-aacr70.rtf
+
+library(tidyverse)
+pkgload::load_all()
+temp_rtf <- tempfile(fileext = ".rtf")
+system.file("extdata", "rt-dm-demo.rtf", package = "artful") |>
+  read_file() |>
+  rtf_indentation() |>
+  write_file(temp_rtf)
+
+df <- rtf_to_html(temp_rtf) |>
+  html_to_dataframe() |>
+  strip_pagination()
+
+df |>
+  mutate(nbsp_count = str_count(df[[1]], fixed("&nbsp;"))) |>
+  mutate(group = cumsum(nbsp_count == 0)) |>
+  mutate(
+    counter = case_when(
+      nbsp_count == 0 ~ 0,
+      nbsp_count > lag(nbsp_count, default = 0) ~ 1,
+      nbsp_count == lag(nbsp_count, default = 0) ~ 0,
+      nbsp_count < lag(nbsp_count, default = 0) ~ -1
+    )
+  ) |>
+  mutate(indent_level = cumsum(counter), .by = group) |>
+  mutate(
+    indent_level_rev = max(indent_level) + min(indent_level) - indent_level,
+    .by = group
+  ) |>
+  rename(value = 1) |>
+  mutate(variable_label = paste0("variable_label", indent_level_rev)) |>
+  mutate(id = row_number()) |> # prevents `pivot_wider()` errors caused by duplicate values
+  pivot_wider(
+    names_from = variable_label,
+    values_from = value,
+    id_cols = everything() # Keep all existing columns for the pivot
+  ) |>
+  fill(starts_with("variable_label")) |>
+  rename(variable_level = variable_label0) |>
+  select(
+    variable_level,
+    starts_with("variable_label"),
+    everything(),
+    -id,
+    -nbsp_count,
+    -group,
+    -counter,
+    -indent_level,
+    -indent_level_rev
+  ) |>
+  mutate(
+    across(
+      everything(),
+      ~ str_replace_all(., fixed("&nbsp;"), "")
+    )
+  )
