ce7200175860370017fa4463ceea3b12e851d299
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-07-03 17:17:58 +0200

    Add slide

--- inst/scripts/topline-slide-tables.R
+++ inst/scripts/topline-slide-tables.R
@@ -107,6 +107,7 @@ rt_dm_demo <- rt_dm_demo |>
 rt_dm_demo <- bind_rows(big_n, rt_dm_demo) |>
   select(starts_with("group"), starts_with("variable"), starts_with("stat"))
 
+print(rt_dm_demo)
 
 # ---- rt-dm-basedz.rtf ----
 temp_rtf <- tempfile(fileext = ".rtf")
@@ -193,8 +194,75 @@ rt_dm_basedz <- rt_dm_basedz |>
 rt_dm_basedz <- bind_rows(big_n, rt_dm_basedz) |>
   select(starts_with("group"), starts_with("variable"), starts_with("stat"))
 
+print(rt_dm_basedz)
+
 # ---- Slide 8 -----------------------------------------------------------------
 # rt-ds-pretrt.rtf
+temp_rtf <- tempfile(fileext = ".rtf")
+
+system.file("extdata", "rt-ds-pretrt.rtf", package = "artful") |>
+  read_file() |>
+  rtf_indentation() |>
+  write_file(temp_rtf)
+
+rt_ds_pretrt <- rtf_to_html(temp_rtf) |>
+  html_to_dataframe() |>
+  manage_exceptions() |>
+  strip_pagination() |>
+  strip_indentation() |>
+  pivot_group()
+
+# Parse stats
+rt_ds_pretrt <- rt_ds_pretrt |>
+  mutate(
+    .id = dplyr::row_number(),
+    stat_list = str_extract_all(stat, "[\\d.]+")
+  ) |>
+  mutate(
+    n_values = lengths(stat_list)
+  ) |>
+  unnest(stat_list) |>
+  mutate(
+    stat_name = case_when(
+      n_values > 1 & row_number() == 1 ~ "n",
+      n_values > 1 & row_number() == 2 ~ "p",
+      .default = NA
+    ),
+    stat = stat_list,
+    .by = .id
+  ) |>
+  select(
+    -c(.id, stat_list, n_values)
+  ) |>
+  left_join(stat_lookup)
+
+big_n <- rt_ds_pretrt |>
+  distinct(group1_level) |>
+  separate_wider_regex(
+    group1_level,
+    patterns = c(
+      variable_label1 = ".*?\\S+",
+      "\\s+",
+      "(?:\\(N = |N = )",
+      stat = "\\d+",
+      "\\)?"
+    )
+  ) |>
+  mutate(stat_name = "N_header", stat_label = "N", .before = "stat")
+
+rt_ds_pretrt <- rt_ds_pretrt |>
+  mutate(
+    group1_level = stringr::str_extract(
+      group1_level,
+      ".*?\\S+(?=\\s*(?:\\(N = |N = ))"
+    )
+  )
+
+rt_ds_pretrt <- bind_rows(big_n, rt_ds_pretrt) |>
+  select(starts_with("group"), starts_with("variable"), starts_with("stat"))
+
+print(rt_ds_pretrt)
+
 # rt-ds-trtwk16.rtf
 
 # ---- Slide 9 -----------------------------------------------------------------
